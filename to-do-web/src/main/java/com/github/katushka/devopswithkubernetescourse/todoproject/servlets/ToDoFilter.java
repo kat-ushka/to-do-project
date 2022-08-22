7f5ff7ac7b0d0bd895e7d1d3a13c56e9527be24e
Yekaterina Polovinkina <kat.ushka403@gmail.com>
2022-08-22 12:48:58 +0300

    fixes routing

---
+++ to-do-web/src/main/java/com/github/katushka/devopswithkubernetescourse/todoproject/servlets/ToDoFilter.java
@@ -0,0 +1,28 @@
+package com.github.katushka.devopswithkubernetescourse.todoproject.servlets;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/todo")
+public class ToDoFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        httpResponse.sendRedirect(httpRequest.getContextPath() + "/faces/index.xhtml");
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
