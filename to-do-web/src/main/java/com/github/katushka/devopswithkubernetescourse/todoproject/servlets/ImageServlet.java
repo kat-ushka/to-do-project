d0698f437ed7effed30673ecb4628d6997ac6a80
Yekaterina Polovinkina <kat.ushka403@gmail.com>
2022-08-30 11:59:56 +0300

    fixes url

--- to-do-web/src/main/java/com/github/katushka/devopswithkubernetescourse/todoproject/servlets/ImageServlet.java
+++ to-do-web/src/main/java/com/github/katushka/devopswithkubernetescourse/todoproject/servlets/ImageServlet.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-@WebServlet("/todo/images/*")
+@WebServlet("/images/*")
 public class ImageServlet extends HttpServlet {
 
     @Inject
